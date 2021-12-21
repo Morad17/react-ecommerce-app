728a66d7e0189649411cb3c87f456e265fa6fbfd
Morad17 <morad123@hotmail.co.uk>
2021-12-21 22:17:33 +0000

    createing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
-
-
+ 
 
 const Navbar = () => {
     return (
-        <nav className='main-navbar'>
-
+        <nav className="main-navbar">
+            <ul>
+                <li>
+                    <div className="logo">
+                        EsToRe
+                    </div>
+                </li>
+                <li className="nav-main-links">
+                    <a href="#" className="nav-link">Shop</a>
+                    <a href="#" className="nav-link">Login</a>
+                    <a href="#" className="nav-link">Basket</a>
+                    <a href="#" className="nav-link">Contact</a>
+                </li>
+            </ul>
         </nav>
     )
 }

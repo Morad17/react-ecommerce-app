e2f0430e6b14a01bdab610a3c2e9fbc4279ac563
Morad17 <morad123@hotmail.co.uk>
2021-12-21 22:52:17 +0000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
  
+import { Search, Person, Chat, Notifications } from '@mui/icons-material/'
 
 const Navbar = () => {
     return (
@@ -10,11 +11,17 @@ const Navbar = () => {
                         EsToRe
                     </div>
                 </li>
+                <li>
+                    <div className="searchbar">
+                        <Search className="search-icon" />
+                        <input placeholder="search" type="text" className="search-input" />
+                    </div>
+                </li>
                 <li className="nav-main-links">
-                    <a href="#" className="nav-link">Shop</a>
-                    <a href="#" className="nav-link">Login</a>
-                    <a href="#" className="nav-link">Basket</a>
-                    <a href="#" className="nav-link">Contact</a>
+                    <a href="/" className="nav-link">Shop</a>
+                    <a href="/" className="nav-link">Login</a>
+                    <a href="/" className="nav-link">Basket</a>
+                    <a href="/" className="nav-link">Contact</a>
                 </li>
             </ul>
         </nav>

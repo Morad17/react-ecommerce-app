3c3ce07b01436e287828766aaa932c72b6751d15
Morad17 <morad123@hotmail.co.uk>
2021-12-28 23:09:08 +0000

    updateing categories and products

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = ({item}) => {
+    return (
+        <div className="product-container">
+            
+        </div>
+    )
+}
+
+export default Product

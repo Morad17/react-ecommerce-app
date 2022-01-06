42d08f4f84bd8dc61081a1d39e45aba9c9222d84
Morad17 <morad123@hotmail.co.uk>
2022-01-06 19:59:32 +0000

    creating authentication page

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -11,9 +13,9 @@ mongoose
     .catch((err)=> {console.log(err);
 });
 
-app.get("/api/test", ()=> {
-    console.log("Test is Successfull");
-})
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 
 
---
+++ backend/models/Basket.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const BasketSchema = new mongoose.Schema({
+    userId: {type: String , required:true, unique:true},
+    products: [
+        {
+            ProductId:{
+                type:String
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            }
+        }
+    ]
+  },
+  { timestamp: true }
+)
+
+module.exports = mongoose.model("Basket", BasketSchema);
